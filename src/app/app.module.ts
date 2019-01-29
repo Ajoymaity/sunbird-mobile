aa8f6ce64ec3609b80a1ac1c309c37842f530bd1
Ajoymaity <ajoymaity17@gmail.com>
2019-01-29 15:30:54 +0530

    implemented app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import {APP_INITIALIZER, ErrorHandler, NgModule, Provider} from '@angular/core';
+import { APP_INITIALIZER, ErrorHandler, NgModule, Provider } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Events, IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
@@ -44,59 +44,61 @@ export const sunbirdSdkServicesProvidersFactory: () => Provider[] = () => {
   }];
 };
 
-export const sunbirdSdkFactory: (uniqueDeviceID: UniqueDeviceID) => Promise<void> =
-  async (uniqueDeviceID: UniqueDeviceID) => {
-    const deviceId = await uniqueDeviceID.get();
+export const sunbirdSdkFactory: (uniqueDeviceID: UniqueDeviceID) => () => Promise<void> =
+  (uniqueDeviceID: UniqueDeviceID) => {
+    return async () => {
+      const deviceId = await uniqueDeviceID.get();
 
-    SunbirdSdk.instance.init({
-      apiConfig: {
-        baseUrl: 'https://dev.sunbirded.org',
-        user_authentication: {
-          redirectUrl: 'org.sunbird.app.dev://mobile',
-          logoutUrl: '',
-          authUrl: ''
-        },
-        api_authentication: {
-          mobileAppKey: 'sunbird - 0.1',
-          mobileAppSecret: 'd0299ce55a6440eb968b46f355e22504',
-          mobileAppConsumer: 'mobile_device',
-          channelId: 'b00bc992ef25f1a9a8d63291e20efc8d',
-          producerId: 'dev.sunbird.app',
-          deviceId: deviceId
-        },
-        cached_requests: {
-          timeToLive: 2000
-        }
-      },
-      dbContext: {
-        getDBName: () => 'GenieServices.db',
-        getDBVersion: () => 16,
-        getAppMigrationList: () => []
-      },
-      contentServiceConfig: {
-        apiPath: ''
-      },
-      courseServiceConfig: {
-        apiPath: ''
-      },
-      formServiceConfig: {
-        apiPath: '',
-        formFilePath: ''
-      },
-      frameworkServiceConfig: {
-        apiPath: '',
-        frameworkConfigFilePaths: [],
-        channelConfigFilePath: ''
-      },
-      profileServiceConfig: {
-        apiPath: '',
-        searchProfilePath: ''
-      },
-      pageServiceConfig: {
-        apiPath: '',
-        filePath: ''
-      }
-    });
+        SunbirdSdk.instance.init({
+          apiConfig: {
+            baseUrl: 'https://dev.sunbirded.org',
+            user_authentication: {
+              redirectUrl: 'org.sunbird.app.dev://mobile',
+              logoutUrl: '',
+              authUrl: ''
+            },
+            api_authentication: {
+              mobileAppKey: 'sunbird - 0.1',
+              mobileAppSecret: 'd0299ce55a6440eb968b46f355e22504',
+              mobileAppConsumer: 'mobile_device',
+              channelId: 'b00bc992ef25f1a9a8d63291e20efc8d',
+              producerId: 'dev.sunbird.app',
+              deviceId: deviceId
+            },
+            cached_requests: {
+              timeToLive: 2000
+            }
+          },
+          dbContext: {
+            getDBName: () => 'GenieServices.db',
+            getDBVersion: () => 16,
+            getAppMigrationList: () => []
+          },
+          contentServiceConfig: {
+            apiPath: ''
+          },
+          courseServiceConfig: {
+            apiPath: ''
+          },
+          formServiceConfig: {
+            apiPath: '',
+            formFilePath: ''
+          },
+          frameworkServiceConfig: {
+            apiPath: '',
+            frameworkConfigFilePaths: [],
+            channelConfigFilePath: ''
+          },
+          profileServiceConfig: {
+            apiPath: '',
+            searchProfilePath: ''
+          },
+          pageServiceConfig: {
+            apiPath: '',
+            filePath: ''
+          }
+        });
+    };
   };
 
 @NgModule({
@@ -152,7 +154,7 @@ export const sunbirdSdkFactory: (uniqueDeviceID: UniqueDeviceID) => Promise<void
     UniqueDeviceID,
     ...sunbirdSdkServicesProvidersFactory(),
     { provide: ErrorHandler, useClass: IonicErrorHandler },
-    {provide: APP_INITIALIZER, useFactory: sunbirdSdkFactory, deps: [UniqueDeviceID], multi: true}
+    { provide: APP_INITIALIZER, useFactory: sunbirdSdkFactory, deps: [UniqueDeviceID], multi: true }
   ],
   exports: [
     BroadcastComponent
